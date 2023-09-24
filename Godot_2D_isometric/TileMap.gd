cef323828ee30ef6e274eaa10890b2fa6eb036b4
Siliam <harrando.ismail@gmail.com>
2023-09-24 17:31:25 +0200

    Isometric tilemaps

--- Godot_2D_isometric/TileMap.gd
+++ Godot_2D_isometric/TileMap.gd
@@ -1,23 +1,34 @@
 extends TileMap
 
-var GridSize = 4
+var GridSize = 10
 var Dic = {}
+var prev_tile = null
 
-func _read():
-	for x in GridSize:
-		for y in GridSize:
+#0, 0
+#4, -9
+#9, 0
+#4, 9
+
+func _ready():
+	for x in range(GridSize):
+		var h = min(x, GridSize-x) + 1
+		for y in range(-h, h):
 			Dic[str(Vector2(x, y))] = {
 				"Type": "Empty",
 			}
-			set_cell(0, Vector2(x, y), 1, Vector2i(0,0), 0)
+			set_cell(0, Vector2(x, y), 0, Vector2i(0,0), 0)
+
 
 func _process(delta):
 	var tile = local_to_map(get_global_mouse_position())
+	
+	if tile != prev_tile:
+		print(tile)
+		prev_tile = tile
 	
 	for x in GridSize:
 		for y in GridSize:
 			erase_cell(1, Vector2(x, y))
 	
 	if Dic.has(str(tile)):
-		set_cell(1, tile, 0, Vector2i(0,0), 0)
-		print(Dic[str(tile)])
+		set_cell(1, tile, 1, Vector2i(0,0), 0)
