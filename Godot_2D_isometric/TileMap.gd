8ea0b4982263060a3000a39b8073fffacb6e4786
Siliam <harrando.ismail@gmail.com>
2023-09-25 23:48:25 +0200

    Fixing isometric tiles selection

--- Godot_2D_isometric/TileMap.gd
+++ Godot_2D_isometric/TileMap.gd
@@ -10,25 +10,29 @@ var prev_tile = null
 #4, 9
 
 func _ready():
-	for x in range(GridSize):
-		var h = min(x, GridSize-x) + 1
-		for y in range(-h, h):
-			Dic[str(Vector2(x, y))] = {
-				"Type": "Empty",
-			}
-			set_cell(0, Vector2(x, y), 0, Vector2i(0,0), 0)
-
+	for x in range(-5, 7):
+		for y in range(-17, 7):
+			var floor_cell = get_cell_tile_data(0, Vector2i(x, y))
+			if floor_cell != null:
+				Dic[str(Vector2(x, y))] = {"Type": "Empty",}
+	
 
 func _process(delta):
 	var tile = local_to_map(get_global_mouse_position())
 	
 	if tile != prev_tile:
-		print(tile)
+		if prev_tile != null:
+			erase_cell(1, prev_tile)
+		set_cell(1, tile, 0, Vector2i(0, 14), 0)
 		prev_tile = tile
 	
-	for x in GridSize:
-		for y in GridSize:
-			erase_cell(1, Vector2(x, y))
+	if Dic.has(str(tile)) == false:
+		if prev_tile != null:
+			erase_cell(1, prev_tile)
 	
-	if Dic.has(str(tile)):
-		set_cell(1, tile, 1, Vector2i(0,0), 0)
+#	for x in GridSize:
+#		for y in GridSize:
+#			erase_cell(1, Vector2(x, y))
+#
+#	if Dic.has(str(tile)):
+#		set_cell(1, tile, 1, Vector2i(0,0), 0)
