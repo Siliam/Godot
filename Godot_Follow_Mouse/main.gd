082b81872ac2c8b4c6fe336544077de755e7fc52
Ismail <harrando.ismail@gmail.com>
2023-10-06 23:54:01 +0200

    Playing with particles

--- Godot_Follow_Mouse/main.gd
+++ Godot_Follow_Mouse/main.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 var Wisp2 = preload("res://wisp_2.tscn")
-
+var all_wisps = []
+var score = 0 
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -17,8 +18,16 @@ func _on_particle_spawn_timer_timeout():
 func spawn_randow_mini_wisp():
 	var screen_size = get_viewport_rect().size
 	var new_wisp = Wisp2.instantiate()
+	new_wisp.name = "Wisp_" + str(len(all_wisps))
+	all_wisps.append(new_wisp)
 	new_wisp.position.x = randi_range(0, screen_size.x)
 	new_wisp.position.y = randi_range(0, screen_size.y)
-	print("Generate new wisp at " + str(new_wisp.position))
 	
 	add_child(new_wisp)
+
+func _on_wisp_area_entered(area):
+	print('Collision!')
+	if self.name != "Wisp":
+		area.queue_free() 
+		score += 1
+		print('New score')
