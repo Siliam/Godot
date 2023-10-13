e3f36aff463fbe98dc484d9ec439e4844de4e3fb
Siliam <harrando.ismail@gmail.com>
2023-10-13 22:39:28 +0200

    refactoring

--- Memory/GameManager.gd
+++ Memory/GameManager.gd
@@ -2,10 +2,23 @@ extends Node
 
 var cardBack = preload("res://assets/cards/cardBack_blue2.png")
 
+@onready var Game = get_node("/root/Game/")
+
+var deck = Array()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	fill_deck()
+	deal_deck()
 
+func fill_deck():
+	for suit in range(1, 5):
+		for value in range(1, 14):
+			deck.append(Card.new(suit, value))
+
+func deal_deck():
+	for card in deck:
+		Game.get_node('grid').add_child(card)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- Memory/game.gd
+++ Memory/game.gd
@@ -5,9 +5,7 @@ extends Control
 func _ready():
 #	var c = Card.new(1, 1)
 #	$grid.add_child(c)
-	for suit in range(1, 5):
-		for value in range(1, 14):
-			$grid.add_child(Card.new(suit, value))
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
