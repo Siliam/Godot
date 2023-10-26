b23ade82659c69d4d59f43e42811459ae4f836cc
Siliam <harrando.ismail@gmail.com>
2023-10-26 20:55:51 +0200

    Moving and animating the character

---
+++ Topdown_ARPG/player/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var speed = 50
+@onready var animations = $AnimationPlayer
+
+func handle_input():
+	var move_direction = Input.get_vector( "ui_left", "ui_right","ui_up", "ui_down")
+	self.velocity = move_direction * speed
+
+func update_animation():
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction = "down"
+		if velocity.x < 0:
+			direction = "left"
+		elif velocity.x > 0:
+			direction = "right"
+		elif velocity.y < 0:
+			direction = "up"
+		animations.play("walk_"+direction)
+
+func _physics_process(delta):
+	handle_input()
+	update_animation()
+	move_and_slide()
