b0b71e77a8a5287fee9126f4514019614721049b
Siliam <harrando.ismail@gmail.com>
2023-11-05 17:36:34 +0100

    Adding hearts

--- Topdown_ARPG/player/player.gd
+++ Topdown_ARPG/player/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 @export var speed = 50
 @onready var animations = $AnimationPlayer
 
+@export var max_health: int = 3
+@onready var current_health: int = 3
+
+
 func handle_input():
 	var move_direction = Input.get_vector( "ui_left", "ui_right","ui_up", "ui_down")
 	self.velocity = move_direction * speed
@@ -27,3 +31,13 @@ func _physics_process(delta):
 	handle_input()
 	update_animation()
 	move_and_slide()
+
+
+func _on_hitbox_area_entered(area):
+	if area.name == "Hitbox":
+		current_health -= 1
+	
+		if current_health == 0:
+			current_health = max_health
+		
+		print(current_health)
