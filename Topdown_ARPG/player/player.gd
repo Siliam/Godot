7aed50d9f9be29f6a1fb7ae0dd3896e2ba2b8798
Siliam <harrando.ismail@gmail.com>
2023-11-09 23:30:42 +0100

    enemy collisions

--- Topdown_ARPG/player/player.gd
+++ Topdown_ARPG/player/player.gd
@@ -6,11 +6,13 @@ signal health_changed
 @onready var animations = $AnimationPlayer
 @onready var effects = $Effects
 @onready var reset_timer = $Timer
-
+@onready var hurt_box = $Hitbox
 @export var knockback_force = 500
 
 @export var max_health: int = 3
 @onready var current_health: int = 3
+
+var is_hurt = false
 
 
 func handle_input():
@@ -37,24 +39,37 @@ func _physics_process(delta):
 	handle_input()
 	update_animation()
 	move_and_slide()
+	if is_hurt == false:
+		for area in hurt_box.get_overlapping_areas():
+			if area.name == "Hitbox":
+				hurt_by_enemy(area)
 
 func _ready():
 	effects.play("RESET")
-func _on_hitbox_area_entered(area):
-	if area.name == "Hitbox":
-		current_health -= 1
+
+func hurt_by_enemy(area):
+	current_health -= 1
+	is_hurt = true
+	if current_health == 0:
+		current_health = max_health
 	
-		if current_health == 0:
-			current_health = max_health
-		
-		health_changed.emit(current_health)
-		print(current_health)
-		knockback(area.get_parent().velocity)
-		effects.play("blink_hurt")
-		reset_timer.start()
-		await reset_timer.timout
-		
+	health_changed.emit(current_health)
+	print(current_health)
+	knockback(area.get_parent().velocity)
+	effects.play("blink_hurt")
+	reset_timer.start()
+	await reset_timer.timeout
+	effects.play("RESET")
+	is_hurt = false
+
+func _on_hitbox_area_entered(area):
+	pass
+
+func _on_hitbox_area_exited(area):
+	pass
+	
 func knockback(enemy_velocity):
 	var knockback_direction = -( velocity - enemy_velocity ).normalized() * knockback_force
 	velocity = knockback_direction
 	move_and_slide()
+
