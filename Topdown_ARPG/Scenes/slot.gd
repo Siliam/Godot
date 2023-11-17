0ed39dbc0e25312f816351c661f02231a054ad42
Siliam <harrando.ismail@gmail.com>
2023-11-17 20:37:08 +0100

    Stacking items

--- Topdown_ARPG/Scenes/slot.gd
+++ Topdown_ARPG/Scenes/slot.gd
@@ -2,12 +2,16 @@ extends Panel
 
 @onready var background_sprite: Sprite2D = $background
 @onready var item_sprite: Sprite2D = $CenterContainer/Panel/item
+@onready var label: Label = $CenterContainer/Panel/Label
 
-func update(item: InventoryItem):
-	if item == null:
+func update(slot: InventorySlot):
+	if slot.item == null:
 		background_sprite.frame = 0
 		item_sprite.visible = false
+		label.visible = false
 	else:
 		background_sprite.frame = 1
 		item_sprite.visible = true
-		item_sprite.texture = item.texture
+		item_sprite.texture = slot.item.texture
+		label.visible = true
+		label.text = 'x' + str(slot.amount)
