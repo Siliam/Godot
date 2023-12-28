8d0827c66e2a9d4c8d071088ddd1ecc48f5687d2
Siliam <harrando.ismail@gmail.com>
2023-12-28 23:49:53 +0100

    Adding healthbar

---
+++ Topdown_ARPG/Scenes/ProgressBar.gd
@@ -0,0 +1,11 @@
+extends ProgressBar
+
+@export var player: Player
+
+func _ready():
+	update()
+	player.health_changed.connect(update)
+	
+func update():
+	value = player.current_health * 100 / player.max_health
+	print("New value", value)
