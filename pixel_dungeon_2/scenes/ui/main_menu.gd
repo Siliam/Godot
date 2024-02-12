92ffd37344d0fd9a4cd49e3354abf0c73dcc3bbd
Siliam <harrando.ismail@gmail.com>
2024-02-12 21:21:47 +0100

    Setting up the character stats

--- pixel_dungeon_2/scenes/ui/main_menu.gd
+++ pixel_dungeon_2/scenes/ui/main_menu.gd
@@ -1,1 +1,16 @@
 extends Control
+
+@onready var continue_button = %Continue
+
+func _ready() -> void:
+	get_tree().paused = false
+
+func _on_continue_pressed():
+	print("Continue run")
+
+func _on_new_run_pressed():
+	print("New run")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
