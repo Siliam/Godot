4dfe966408b1037c3c3e7537a08c0bcdd0ba48dd
Siliam <harrando.ismail@gmail.com>
2024-02-14 22:45:07 +0100

    Setting up the character loader

---
+++ pixel_dungeon_2/scenes/ui/character_selector.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_warrior_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_wizard_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_assassin_button_pressed():
+	pass # Replace with function body.
