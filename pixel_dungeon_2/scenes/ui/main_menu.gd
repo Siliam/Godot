82658bcebb34c8ab2449fd239023fd856a5b0d30
Siliam <harrando.ismail@gmail.com>
2024-02-16 23:58:13 +0100

    Transition to character selection scene

--- pixel_dungeon_2/scenes/ui/main_menu.gd
+++ pixel_dungeon_2/scenes/ui/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const CHAR_SELECTOR_SCENE := preload("res://scenes/ui/character_selector.tscn")
+
 @onready var continue_button = %Continue
 
 func _ready() -> void:
@@ -9,7 +11,7 @@ func _on_continue_pressed():
 	print("Continue run")
 
 func _on_new_run_pressed():
-	print("New run")
+	get_tree().change_scene_to_packed(CHAR_SELECTOR_SCENE)
 
 
 func _on_exit_pressed():
